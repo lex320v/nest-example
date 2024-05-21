a451099da2037e23a5e8ff6c0dd7a0144f8d96a7
lex <lex@lex.lex>
2024-05-21 19:54:40 +0300

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormconfig from './typeormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(typeormconfig), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/typeormconfig.ts
@@ -0,0 +1,12 @@
+import { DataSourceOptions } from 'typeorm';
+
+const config: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'toor',
+  database: 'nest',
+};
+
+export default config;
